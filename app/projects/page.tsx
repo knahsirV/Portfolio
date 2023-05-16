38b1b528c2f82071a20442f9249adab960ee1b63
Vrishank Viswanath <vrishank@utexas.edu>
2023-05-16 18:23:44 -0500

    project grid done, need to add data

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,26 +5,26 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <main className='mx-auto min-h-screen p-6 lowercase md:w-3/4 md:p-10'>
-        <section className='mb-16 grid place-content-center md:h-[45vh]'>
+      <main className='mx-auto min-h-screen max-w-6xl p-6 lowercase md:w-3/4 md:p-10'>
+        <section id='hero' className=' top-20 mb-16 grid place-content-center'>
           <h1 className='mb-6 text-center text-5xl font-bold text-zinc-50 md:text-6xl'>
             my
-            <span className='bg-gradient-to-br from-orange-600 to-indigo-600 bg-clip-text text-transparent'>
+            <span className='bg-gradient-to-br from-fuchsia-600 to-blue-600 bg-clip-text text-transparent'>
               {" portfolio"}
             </span>
           </h1>
           <p className='mx-auto mb-12 max-w-2xl text-center text-lg font-medium text-zinc-400 md:text-xl'>
-            Welcome to my portfolio! I love learning and building projects using various
-            technologies to create dynamic and efficient applications.
+            Here are the projects I've worked on. I'm always looking for new opportunities, so feel
+            free to reach out!
           </p>
-          {/* <div className='flex flex-col flex-wrap items-center gap-4 md:flex-row md:justify-between md:gap-0'>
-            {[...Array(9).keys()].map((project) => (
-              <div key={project}>
-                <Project />
-              </div>
-            ))}
-          </div> */}
         </section>
+        <div className='flex flex-col flex-wrap items-center gap-4 md:flex-row md:justify-around md:gap-8'>
+          {[...Array(9).keys()].map((project) => (
+            <div key={project}>
+              <Project />
+            </div>
+          ))}
+        </div>
       </main>
     </>
   );
