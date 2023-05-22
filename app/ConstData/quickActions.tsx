55931b571f0eb1a423c6c82be0c597feab1e4cd3
Vrishank Viswanath <vrishank@utexas.edu>
2023-05-22 10:41:50 -0500

    added trackable resume link

--- app/ConstData/quickActions.tsx
+++ app/ConstData/quickActions.tsx
@@ -19,7 +19,7 @@ export const quickActions = [
   },
   {
     name: "resume",
-    link: "/",
+    link: "https://homefromcollege.com/r/VV-749",
     icon: <ClipboardDocumentListIcon className='h-4 w-4' />,
   },
 ];
