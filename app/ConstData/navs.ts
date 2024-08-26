bda2da72511fdf602c3a7c845f6d35e817db7916
Vrishank Viswanath <vrishank@utexas.edu>
2024-08-26 15:50:20 -0500

    clear main

--- app/ConstData/navs.ts
+++
@@ -1,6 +0,0 @@
-export const navs = [
-  { name: "Home", link: "/#home" },
-  // { name: "About", link: "/about" },
-  { name: "Skills", link: "/#skills" },
-  { name: "Projects", link: "/projects" },
-];
--- app/components/LatestProjects.tsx
+++
@@ -1,9 +0,0 @@
-const LatestProjects = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className='flex snap-x items-center gap-4 md:flex-row md:items-start md:justify-between md:gap-0'>
-      {children}
-    </div>
-  );
-};
-
-export default LatestProjects;
